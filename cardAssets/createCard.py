11aa89170dcb97e943919483db3010f1c7acfb25
Sarim-K <shktv03@gmail.com>
2020-01-25 20:06:48 +0000

    fixed some datatypes fixed bug (trying to divide by 0)

--- cardAssets/createCard.py
+++ cardAssets/createCard.py
@@ -2,24 +2,30 @@ from PIL import Image, ImageDraw, ImageFont
 import requests
 
 def createCard(upvotes,downvotes,name,avatar_url):
+    upvotes,downvotes = int(upvotes),int(downvotes)
+
     karma = upvotes-downvotes
     red_x1,red_y1,red_x2,red_y2 = 294,113,646,159
     bar_size = [red_x2-red_x1,red_y2-red_y1]
     
-    total_vote = upvotes+downvotes
-    upvote_percentage = (upvotes/total_vote)
+    total_vote = int(upvotes)+int(downvotes)
+    try:
+        upvote_percentage = (upvotes/total_vote)
+    except:
+        upvote_percentage = 0
 
-    img = Image.open("template.png")
+    img = Image.open("cardAssets/template.png")
     canvas = Image.new('RGBA', (700, 250), (0, 0, 0, 0))
     canvas.paste(img, (0,0))
 
     rimg_draw = ImageDraw.Draw(canvas)
     rimg_draw.rectangle((red_x1,red_y1,red_x2,red_y2), "#FF463D")
 
-    green_x2 = red_x1+(bar_size[0]*upvote_percentage)
-    rimg_draw.rectangle((red_x1,red_y1,green_x2,red_y2), "#70FF32")
+    if upvote_percentage > 0:
+        green_x2 = red_x1+(bar_size[0]*upvote_percentage)
+        rimg_draw.rectangle((red_x1,red_y1,green_x2,red_y2), "#70FF32")
 
-    img = Image.open("bar_overlay.png").convert("RGBA")
+    img = Image.open("cardAssets/bar_overlay.png").convert("RGBA")
     canvas.paste(img, (0, 0), img)
 
     img = Image.open(requests.get(avatar_url, stream=True).raw)
@@ -27,17 +33,14 @@ def createCard(upvotes,downvotes,name,avatar_url):
     img.thumbnail(size)
     canvas.paste(img, (15,12))
 
-    img = Image.open("circle_overlay.png").convert("RGBA")
+    img = Image.open("cardAssets/circle_overlay.png").convert("RGBA")
     canvas.paste(img, (0,0), img)
     
 ###########################################################################
     
-    W, H = (352,46)
     if karma>0:
         karma = "+"+str(karma)
     
-    msg = f"{name}        {karma}"
-
     arial_font = ImageFont.truetype('arial.ttf', 40)
     rimg_draw.text((294, 60),f"{name} {karma}","#CECECE",font=arial_font)
 
@@ -58,7 +61,3 @@ def createCard(upvotes,downvotes,name,avatar_url):
     rimg_draw.text(karma_coords,f"{downvotes}","#FF463D",font=arial_font)
 
     canvas.save("card.png")
-
-
-createCard(106,23,"sarim","https://cdn.discordapp.com/avatars/297111993352060948/4109edce51d37d9ede756bea4e8059e9.jpg?size=1024")
-
