75e5b27c2cc90ceaf69e8d0567dd90c66bf44493
Sarim-K <shktv03@gmail.com>
2020-01-25 16:04:26 +0000

    now removes from db when reaction is removed

--- runBot.py
+++ runBot.py
@@ -52,7 +52,7 @@ async def on_message(message):
     discordname = str(message.author.name)+"#"+str(message.author.discriminator)
 
     # -----------------------------------------------------------   
-    #                      Get Karma Command
+    #                      View Karma Command
     # -----------------------------------------------------------
 	
     if usermessage.startswith("$karma"):
@@ -73,7 +73,7 @@ async def on_message(message):
             individual_user_data = db_c.fetchall()
             individual_user_data = str(individual_user_data[0]).replace("(","").replace(")","").replace(" ","").split(",")
         except:
-            individual_user_data = ["NO DATA","NO DATA","NO DATA"]
+            individual_user_data = [user_id_to_use,0,0]
 
         title,description,colour=f"{username_to_use}'s karma:",f"{individual_user_data[1]}/{individual_user_data[2]}",0x4BB543
         embed = discord.Embed(title=title,description=description,color=colour)
@@ -124,6 +124,50 @@ async def on_raw_reaction_add(payload):
 
         upvotes = int(individual_user_data[1])+upvote
         downvotes = int(individual_user_data[2])+downvote
+        data_tuple = (msg.author.id,upvotes,downvotes)
+        db_c.execute(sqlite_insert_with_param, data_tuple)
+        db_connection.commit()
+        db_connection.close()
+
+@client.event
+async def on_raw_reaction_remove(payload):
+    upvote,downvote = 0,0
+    if payload.emoji.id == upvote_emoji:
+        upvote = 1
+    elif payload.emoji.id == downvote_emoji:
+        downvote = 1
+    else:
+        return
+
+    channel = client.get_channel(payload.channel_id)
+    try:
+        msg = await channel.fetch_message(payload.message_id)
+    except NotFound:
+        print("Message not found.")
+        return
+    except Exception as e:
+        print(f"Unknown error.\n{e}")
+
+    db_connection = sqlite3.connect('user_data.db')
+    db_c = db_connection.cursor()
+
+    db_c.execute("SELECT * FROM userdata WHERE userid=?", (msg.author.id,))
+    individual_user_data = db_c.fetchall()
+
+    if individual_user_data == []:
+        print("No user data to remove reaction from")
+        return
+
+    else:
+        individual_user_data = str(individual_user_data[0]).replace("(","").replace(")","").replace(" ","").split(",")
+        
+        db_c.execute("DELETE from userdata where userid = ?",(msg.author.id,))
+        sqlite_insert_with_param = """INSERT INTO userdata
+                            (userid, upvotes, downvotes) 
+                            VALUES (?, ?, ?);"""
+
+        upvotes = int(individual_user_data[1])-upvote
+        downvotes = int(individual_user_data[2])-downvote
         data_tuple = (msg.author.id,upvotes,downvotes)
         db_c.execute(sqlite_insert_with_param, data_tuple)
         db_connection.commit()
